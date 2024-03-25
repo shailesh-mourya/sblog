5ad16e57d7eda3bdb10dc67955e1f9192fa52262
shailesh mourya <ssm.2000mourya@gmail.com>
2024-03-26 00:09:39 +0530

    v1 create home & admindashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:"home",pathMatch:"full"},
+    {path:"home",component:HomeComponent},
+    {path:'',children:[
+        {path:"admin-dashboard",component:AdminDashboardComponent}
+    ]}
+    
+];
