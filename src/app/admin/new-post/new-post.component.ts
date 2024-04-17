b6a762553c3c54ccd8d85b2c0a8e6d6a7daeb169
shailesh mourya <ssm.2000mourya@gmail.com>
2024-04-17 14:09:59 +0530

    v5 created post-service

--- src/app/admin/new-post/new-post.component.ts
+++ src/app/admin/new-post/new-post.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PostServiceService } from '../../post-service/post-service.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
+  postsForm!:FormGroup;
+
+
+
+  constructor(private fb2:FormBuilder,private np:PostServiceService){
+    this.postsForm = this.fb2.group({
+      title:['',Validators.required],
+      description :['',Validators.required],
+      content :['',Validators.required],
+      
+    })
+  }
+
+
+  Add(){
+    this.np.addPost(this.postsForm.value).subscribe((data)=>{
+      console.log('post added');
+      
+    })
+    
+  }
 
 }
---
+++ src/app/post-service/post-service.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostServiceService {
+   //1
+   private url = 'http://localhost:3000';
+
+  //2
+  constructor(private http:HttpClient) { }
+
+  
+  //3 methods which we call in components inside
+  getPost():Observable<any>{
+    return this.http.get(`${this.url}/post/showpost`)
+  }
+
+  //
+  addPost(posts:any):Observable<any>{
+    return this.http.post(`${this.url}/post/addpost`,posts);
+  }
+
+  deletePost(title:any):Observable<any>{
+    return this.http.delete(`${this.url}/post/deletepost/${title}`);
+  }
+
+}
